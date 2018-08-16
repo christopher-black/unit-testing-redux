37f6d18f1ece90e4a2863be3ae1e8ecf827a168c
Chris Black <chris@primeacademy.io>
2018-08-15 21:22:34 -0500

    Fixed tests.

--- test/counter.test.js
+++ test/counter.test.js
@@ -5,9 +5,9 @@ import counterReducer from '../src/counter.reducer.js';
 let store;
 
 describe('counter reducer', function() {
-
+  // runs before all tests in this block
   beforeEach(function() {
-    // runs before all tests in this block
+    // this resets the store between each test
     store = createStore(counterReducer);
   });
 
@@ -15,13 +15,24 @@ describe('counter reducer', function() {
       expect(store.getState().counterReducer).to.equal(0);
   });
 
-  it('should equal one after dispatching ADD', function(){
-      store.dispatch({type: 'ADD'});
-      expect(store.getState().counterReducer).to.equal(1);
+  it('should be able to ADD', function(){
+    store.dispatch({type: 'ADD'});
+    expect(store.getState().counterReducer).to.equal(1);
+    store.dispatch({type: 'ADD'});
+    expect(store.getState().counterReducer).to.equal(2);
   });
 
-  it('should equal negative after dispatching SUBTRACT', function(){
-      store.dispatch({type: 'SUBTRACT'});
-      expect(store.getState().counterReducer).to.equal(-1);
+  it('should be able to SUBTRACT', function(){
+    store.dispatch({type: 'SUBTRACT'});
+    expect(store.getState().counterReducer).to.equal(-1);
+    store.dispatch({type: 'SUBTRACT'});
+    expect(store.getState().counterReducer).to.equal(-2);
+  });
+
+  it('should be able to ADD & SUBTRACT', function(){
+    store.dispatch({type: 'ADD'});
+    expect(store.getState().counterReducer).to.equal(1);
+    store.dispatch({type: 'SUBTRACT'});
+    expect(store.getState().counterReducer).to.equal(0);
   });
 });
